711defbb4ab914f9b6f53373d8900013f1168be1
Yannan Zhang <zhangyannan199744@outlook.com>
2024-01-16 21:56:54 +0200

    1/16 3&4 reset

--- e03/E03.java
+++ e03/E03.java
@@ -3,19 +3,17 @@ import java.util.Scanner;
 
 class E03 {
     public static void main(String[] args) {
-        for (int i = 0; i < 2 ; i ++) {
-            System.out.println("Enter a number:");
-            try {
-                Scanner scanner = new Scanner(System.in);
-                double number = scanner.nextDouble();
-                if (number == 0) {
-                    System.out.println("Zero number");
-                } else {
-                    System.out.println(number > 0 ? "Positive number" : "Negative number");
-                }
-            } catch (InputMismatchException e) {
-                System.out.println("Input a number!");
+        System.out.println("Enter a number:");
+        try {
+            Scanner scanner = new Scanner(System.in);
+            double number = scanner.nextDouble();
+            if (number == 0) {
+                System.out.println("Zero number");
+            } else {
+                System.out.println(number > 0 ? "Positive number" : "Negative number");
             }
+        } catch (InputMismatchException e) {
+            System.out.println("Input a number!");
         }
     }
 }
--- e04/E04.java
+++ e04/E04.java
@@ -3,27 +3,25 @@ import java.util.Scanner;
 
 class E04 {
     public static void main(String[] args) {
-        for (int i = 0; i < 2 ; i ++) {
-            System.out.println("Enter a day:");
-            try {
-                Scanner scanner = new Scanner(System.in);
-                int day = scanner.nextInt();
-                if (day < 1 || day > 31) {
-                    System.out.println("Out of range: 1 - 31");
+        System.out.println("Enter a day:");
+        try {
+            Scanner scanner = new Scanner(System.in);
+            int day = scanner.nextInt();
+            if (day < 1 || day > 31) {
+                System.out.println("Out of range: 1 - 31");
+            } else {
+                System.out.println("Enter a month:");
+                int month = scanner.nextInt();
+                if (month < 1 || month > 12) {
+                    System.out.println("Out of range: 1 - 12");
                 } else {
-                    System.out.println("Enter a month:");
-                    int month = scanner.nextInt();
-                    if (month < 1 || month > 12) {
-                        System.out.println("Out of range: 1 - 12");
-                    } else {
-                        if (day == 24 && month == 12) {
-                            System.out.println("Merry Christmas");
-                        }
+                    if (day == 24 && month == 12) {
+                        System.out.println("Merry Christmas");
                     }
                 }
-            } catch (InputMismatchException e) {
-                System.out.println("Input a int value!");
             }
+        } catch (InputMismatchException e) {
+            System.out.println("Input a int value!");
         }
     }
 }
