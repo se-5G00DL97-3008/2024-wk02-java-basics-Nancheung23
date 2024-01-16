bac33a442ae94355de1a12e0d3ae60d623315344
Yannan Zhang <zhangyannan199744@outlook.com>
2024-01-16 22:01:53 +0200

    1/16 3&4 correct

--- e03/E03.java
+++ e03/E03.java
@@ -3,14 +3,14 @@ import java.util.Scanner;
 
 class E03 {
     public static void main(String[] args) {
-        System.out.println("Enter a number:");
+        System.out.println("Enter a number: ");
         try {
             Scanner scanner = new Scanner(System.in);
             double number = scanner.nextDouble();
             if (number == 0) {
                 System.out.println("Zero number");
             } else {
-                System.out.println(number > 0 ? "Positive number" : "Negative number");
+                System.out.print(number > 0 ? "Positive number\n" : "Negative number\n");
             }
         } catch (InputMismatchException e) {
             System.out.println("Input a number!");
